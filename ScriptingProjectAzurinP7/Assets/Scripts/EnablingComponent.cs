abf59811f21237a327416f3869ffffd559e60ce2
DrewAzurinP7 <740723@student.dusd.net>
2023-01-11 15:27:35 -0800

    Deleted Semester 1 scripts
    Added first semester 2 script

--- ScriptingProjectAzurinP7/Assets/Scripts/EnablingComponent.cs
+++ ScriptingProjectAzurinP7/Assets/Scripts/EnablingComponent.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AwakeAndStart : MonoBehaviour
+public class EnablingComponent : MonoBehaviour
 {
-    private void Awake()
-    {
-        Debug.Log("Awake called.");
-    }
+    private Light myLight;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start called.");
+        myLight = GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
     }
 }
