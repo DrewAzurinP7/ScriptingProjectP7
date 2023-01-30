61d47ef9688b1b19c2b46dfceb74a59b2a6d5bc5
DrewAzurinP7 <740723@student.dusd.net>
2023-01-30 15:26:27 -0800

    Added Linear Interpolation
    Added Linear Interpolation to Scripting Project

---
+++ ScriptingProjectAzurinP7/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    //In this case,, result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+    //Mathf.Lerp function takes 3 float parameters: One represents the value to interpolate from; another represents the value to interpolate to & a final float represting how far to interpolate.
+
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+    //Here result = (4, 5, 6)
+    Vector3 results = Vector3.Lerp(from, to, 0.75f);
+    //The 4 represent 75% between 1 & 5 &, & 5 represents 75% of the way between 2 & 6
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Light.intensity = Mathf.Lerp(Light.intensity, 8f, 0.5f);
+        //Every frame that runs will be set to 0, then to 4, then to 6, then to 7, then to 7.5 & so on
+
+        //However, if we add Time.deltaTime, the frame will happen per second instead of per frame
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+    */
+}
